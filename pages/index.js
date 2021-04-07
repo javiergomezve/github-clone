13b545b5faf3ae399c6751811e861e18ed3c20bb
Javier Gomez <javiergomezve@gmail.com>
2021-04-07 11:06:05 -0500

    [ADD] File explorer

--- pages/index.js
+++ pages/index.js
@@ -188,6 +188,212 @@ export default function Home() {
                     </a>
                 </li>
             </ul>
+
+            <div className="container mx-auto my-8 px-4 flex">
+                <div className="file-explorer-container w-3/4 mr-8">
+                    <div className="branch-navigation flex items-center justify-between">
+                        <div className="flex items-center space-x-2">
+                            <button className="border border-gray-400 rounded-md px-4 py-1 hover:bg-gray-200 flex items-center space-x-2">
+                                <svg className="w-4" viewBox="0 0 16 16" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M11.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122V6A2.5 2.5 0 0110 8.5H6a1 1 0 00-1 1v1.128a2.251 2.251 0 11-1.5 0V5.372a2.25 2.25 0 111.5 0v1.836A2.492 2.492 0 016 7h4a1 1 0 001-1v-.628A2.25 2.25 0 019.5 3.25zM4.25 12a.75.75 0 100 1.5.75.75 0 000-1.5zM3.5 3.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0z"></path>
+                                </svg>
+
+                                <span className="font-semibold">master</span>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                                </svg>
+                            </button>
+
+                            <a href="#" className="flex items-center space-x-2 px-3 py-1 group">
+                                <svg className="w-4 fill-current text-gray-600 group-hover:text-blue-600" viewBox="0 0 16 16" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M11.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122V6A2.5 2.5 0 0110 8.5H6a1 1 0 00-1 1v1.128a2.251 2.251 0 11-1.5 0V5.372a2.25 2.25 0 111.5 0v1.836A2.492 2.492 0 016 7h4a1 1 0 001-1v-.628A2.25 2.25 0 019.5 3.25zM4.25 12a.75.75 0 100 1.5.75.75 0 000-1.5zM3.5 3.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0z"></path>
+                                </svg>
+
+                                <span className="text-gray-600 group-hover:text-blue-600">
+                                    <span className="font-semibold">
+                                        25{' '}
+                                    </span>
+                                    <span>branches</span>
+                                </span>
+                            </a>
+
+                            <a href="#" className="flex items-center space-x-2 px-3 py-1 group">
+                                <svg className="w-4 fill-current text-gray-600 group-hover:text-blue-600" viewBox="0 0 16 16" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z"></path>
+                                </svg>
+
+                                <span className="text-gray-600 group-hover:text-blue-600">
+                                    <span className="font-semibold">
+                                        2{' '}
+                                    </span>
+                                    <span>tags</span>
+                                </span>
+                            </a>
+                        </div>
+
+                        <div className="flex items-center space-x-3">
+                            <button className="border border-gray-400 rounded-md text-black hover:bg-gray-100 px-4 py-1 flex items-center space-x-1">
+                                Go to file
+                            </button>
+
+                            <button className="border border-gray-400 rounded-md text-black hover:bg-gray-100 px-4 py-1 flex items-center space-x-1">
+                                <span>
+                                    Add file
+                                </span>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                                </svg>
+                            </button>
+
+                            <button className="border border-gray-400 text-white rounded-md bg-green-600 hover:bg-green-700 px-4 py-1 flex items-center space-x-1">
+                                <svg className="fill-current w-4" viewBox="0 0 16 16" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M7.47 10.78a.75.75 0 001.06 0l3.75-3.75a.75.75 0 00-1.06-1.06L8.75 8.44V1.75a.75.75 0 00-1.5 0v6.69L4.78 5.97a.75.75 0 00-1.06 1.06l3.75 3.75zM3.75 13a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5z"></path>
+                                </svg>
+
+                                <span>
+                                    Code
+                                </span>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="commits-container bg-blue-100 rounded-md rounded-b-none border border-blue-200 border-b-0 flex items-center justify-between px-4 py-4 mt-5">
+                        <div className="flex items-center space-x-2">
+                            <a href="#" className="flex items-center hover:text-gray-400">
+                                <img src="https://avatars.githubusercontent.com/u/7153989?s=60&amp;v=4" alt="@javiergomezve"
+                                     className="w-6 h-6 rounded-full" />
+                            </a>
+                            <a href="#" className="font-semibold hover:underline">
+                                javiergomezve
+                            </a>
+                            <a href="#" className="hover:underline hover:text-blue-600">
+                                1.5.1
+                            </a>
+                        </div>
+
+                        <div className="flex items-center space-x-2">
+                            <a href="#">
+                                <svg aria-label="0 / 2 checks OK" className="w-4 fill-current text-red-700" viewBox="0 0 16 16" role="img">
+                                    <path fill-rule="evenodd"
+                                          d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path>
+                                </svg>
+                            </a>
+
+                            <a href="#"
+                               className="font-mono text-xs text-gray-600 hover:underline hover:text-blue-500 ml-2">
+                                da070bd
+                            </a>
+
+                            <a href="#" className="text-gray-600 hover:underline hover:text-blue-500 ml-2">
+                                4 days ago
+                            </a>
+
+                            <a href="#" className="text-black px-4 py-1 flex items-center space-x-1 hover:text-blue-600">
+                                <svg className="w-4 fill-current" viewBox="0 0 16 16"
+                                     version="1.1" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M1.643 3.143L.427 1.927A.25.25 0 000 2.104V5.75c0 .138.112.25.25.25h3.646a.25.25 0 00.177-.427L2.715 4.215a6.5 6.5 0 11-1.18 4.458.75.75 0 10-1.493.154 8.001 8.001 0 101.6-5.684zM7.75 4a.75.75 0 01.75.75v2.992l2.028.812a.75.75 0 01-.557 1.392l-2.5-1A.75.75 0 017 8.25v-3.5A.75.75 0 017.75 4z"></path>
+                                </svg>
+
+                                <span className="font-semibold">
+                                    3,672
+                                </span>
+
+                                <span>
+                                    commits
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="file-explorer rounded-md rounded-t-none border border-gray-400 text-gray-700 divide-y divide-gray-400">
+                        <div className="flex justify-between px-4 py-2 hover:bg-gray-100">
+                            <div className="w-4/12 flex items-center">
+                                <svg aria-label="Directory" className="w-4 fill-current text-blue-400" viewBox="0 0 16 16" role="img">
+                                    <path fill-rule="evenodd"
+                                          d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3h-6.5a.25.25 0 01-.2-.1l-.9-1.2c-.33-.44-.85-.7-1.4-.7h-3.5z"></path>
+                                </svg>
+
+                                <a href="#" className="hover:underline hover:text-blue-500 ml-3">.github</a>
+                            </div>
+                            <div className="w-6/12 truncate">
+                                <a href="#" className="text-gray-600 hover:underline hover:text-blue-600">
+                                    Initial commit
+                                </a>
+                            </div>
+                            <div className="w-2/12 text-right">
+                                <span className="text-gray-600">5 days ago</span>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between px-4 py-2 hover:bg-gray-100">
+                            <div className="w-4/12 flex items-center">
+                                <svg aria-label="File" className="w-4" viewBox="0 0 16 16" role="img">
+                                    <path fill-rule="evenodd"
+                                          d="M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z"></path>
+                                </svg>
+
+                                <a href="#" className="hover:underline hover:text-blue-500 ml-3">index.js</a>
+                            </div>
+                            <div className="w-6/12 truncate">
+                                <a href="#" className="text-gray-600 hover:underline hover:text-blue-600">
+                                    Initial commit
+                                </a>
+                            </div>
+                            <div className="w-2/12 text-right">
+                                <span className="text-gray-600">5 days ago</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="readme-container rounded-md border border-gray-400 px-4 py-4 mt-4">
+                        <h4 className="font-semibold">README.md</h4>
+
+                        <div>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto deleniti, eius et labore magni molestiae, molestias nisi quaerat recusandae, ut velit veritatis voluptatem. Commodi necessitatibus non quam, quis quisquam soluta. Aperiam asperiores aspernatur blanditiis delectus deleniti distinctio dolor dolorem ea error esse mollitia, nisi quaerat quasi quos ratione reiciendis sapiente sed ullam? Deleniti fuga magni odit quia rerum! Cupiditate eum fugiat maxime, minima numquam odio perferendis quisquam sapiente temporibus voluptates! Animi error, incidunt molestiae recusandae sed veniam. A aliquam assumenda beatae dignissimos dolorem et fugiat illo impedit inventore ipsum iure laudantium nobis praesentium, quas, quos recusandae, rerum saepe veritatis?
+                        </div>
+                    </div>
+                </div>
+
+                <div className="sidebar w-1/4">
+
+                </div>
+            </div>
+
+            <footer className="container mx-auto text-xs border-t border-gray-400 py-10 flex items-center justify-between">
+                <ul className="flex items-center space-x-12">
+                    <li>&copy; 2020 GitHub, Inc.</li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Terms</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Privacy</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Security</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Status</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Docs</a></li>
+                </ul>
+
+                <a href="#">
+                    <svg className="w-4 fill-current text-gray-400 hover:text-gray-700" viewBox="0 0 16 16" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+                    </svg>
+                </a>
+
+                <ul className="flex items-center space-x-12">
+                    <li><a href="#" className="text-blue-600 hover:underline">Contact GitHub</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Pricing</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">API</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Training</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">Blog</a></li>
+                    <li><a href="#" className="text-blue-600 hover:underline">About</a></li>
+                </ul>
+            </footer>
         </div>
     );
 }
